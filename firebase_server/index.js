26429cd8244b46ae0ae26464341d23acdc349f0c
Cameron Barker <cameronbarkeran@gmail.com>
2022-01-31 22:32:23 +0000

    serial

---
+++ firebase_server/index.js
@@ -0,0 +1,19 @@
+
+var index = fs.readFileSync( 'index.html');
+
+var SerialPort = require('serialport');
+const parsers = SerialPort.parsers;
+
+const parser = new parsers.Readline({
+    delimiter: '\r\n'
+});
+
+var port = new SerialPort('COM4',{ 
+    baudRate: 9600,
+    dataBits: 8,
+    parity: 'none',
+    stopBits: 1,
+    flowControl: false
+});
+
+port.pipe(parser);
