451cd203237553945a9376e5faac8dfff23a7896
Cameron Barker <cameronbarkeran@gmail.com>
2022-02-06 21:08:33 +0000

    Shelf switch

--- shelf/shelf.ino
+++ shelf/shelf.ino
@@ -1,14 +1,17 @@
-
-
 int sensorPin = A0;
+int prev;
+int cur;
 
 void setup() {
-  // declare the ledPin as an OUTPUT:
   Serial.begin(9600);
 }
 
 void loop() {
   // read the value from the sensor:
-  Serial.println(analogRead(sensorPin));
+  cur = analogRead(sensorPin);
+  if (cur != prev) {
+    Serial.println(cur);
+    prev = cur;
+  }
   delay(200);
 }
