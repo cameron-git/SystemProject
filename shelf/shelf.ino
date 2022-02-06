b2d3c40363ec4f28e086467d22d93a50f1297a1b
Cameron Barker <cameronbarkeran@gmail.com>
2022-02-06 23:16:09 +0000

    1

--- shelf/shelf.ino
+++ shelf/shelf.ino
@@ -11,19 +11,11 @@ void loop()
 {
   // read the value from the sensor:
   cur = analogRead(sensorPin);
-  if (cur < 512)
-  {
-    cur = 0;
-  }
-  else
-  {
-    cur = 1;
-  }
 
   if (cur != prev)
   {
     Serial.println(cur);
     prev = cur;
   }
-  delay(500);
+  delay(200);
 }
