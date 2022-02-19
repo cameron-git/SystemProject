f1b7e3ed8418b31efdbe871a6ce03a1685e9b9f1
Cameron Barker <cameronbarkeran@gmail.com>
2022-02-19 01:20:37 +0000

    app small update

--- inventory_app/lib/main.dart
+++ inventory_app/lib/main.dart
@@ -1,7 +1,8 @@
+import 'package:firebase_database/ui/firebase_animated_list.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_database/firebase_database.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -12,8 +13,19 @@ void main() async {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  DatabaseReference dbref = FirebaseDatabase.instanceFor(
+          app: Firebase.app(),
+          databaseURL:
+              'systemprojectgroup1-default-rtdb.europe-west1.firebasedatabase.app')
+      .ref();
 
   @override
   Widget build(BuildContext context) {
@@ -23,37 +35,14 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: const Text('Inventory'),
         ),
-        body: Center(
-          child: StreamBuilder(
-            // need to handle loading
-            stream:
-                FirebaseFirestore.instance.collection('shelves').snapshots(),
-            builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
-              if (!snapshot.hasData) {
-                return Container();
-              }
-              return ListView(
-                children: snapshot.data!.docs.map(
-                  (e) {
-                    return Card(
-                      child: Padding(
-                        padding: const EdgeInsets.all(30),
-                        child: Column(
-                          children: [
-                            Text(e.id),
-                            const SizedBox(height: 8),
-                            Text('contents: ' + e['contents'].toString()),
-                            const SizedBox(height: 8),
-                            Text('capacity: ' + e['capacity'].toString()),
-                          ],
-                        ),
-                      ),
-                    );
-                  },
-                ).toList(),
-              );
-            },
-          ),
+        body: FirebaseAnimatedList(
+          query: dbref,
+          itemBuilder: (context, snapshot, animation, index) {
+            var sn = snapshot.value as int;
+            return ListTile(
+              title: Text(sn.toString()),
+            );
+          },
         ),
       ),
     );
