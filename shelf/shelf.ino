08beff927db5ccecff571718139d75b66baee057
Cameron Barker <cameronbarkeran@gmail.com>
2022-02-20 16:08:05 +0000

    iot 33 shelf

--- shelf/shelf.ino
+++ shelf/shelf.ino
@@ -1,64 +1,53 @@
 #include <ArduinoJson.h>
 #include "Firebase_Arduino_WiFiNINA.h"
 
+#define DATABASE_URL "systemprojectgroup1-default-rtdb.europe-west1.firebasedatabase.app" //<databaseName>.firebaseio.com or <databaseName>.<region>.firebasedatabase.app
+#define DATABASE_SECRET "l3ONLDlbr7x1zRP7Y8a0UOA8UNH1PL6VDCilozTU"
+#define WIFI_SSID "VM7136878"
+#define WIFI_PASSWORD "dryv5qrKqsc7"
+
 FirebaseData fbdo;
 
-int sensorPin = A0;
-int prev;
-int cur;
+char jsonData[512];
 
 void setup()
 {
   Serial.begin(9600);
-  while (!Serial) ;
-  
-  Serial.println("start");
+  while (!Serial)
+  {
+  };
 
-  Firebase.begin("systemprojectgroup1-default-rtdb.europe-west1.firebasedatabase.app", "kecPXGK9y9C6ecdNcVYLouU4lGzbTZ7AM09bx1Om", "S10", "camerons10");
-  
+  Firebase.begin(DATABASE_URL, DATABASE_SECRET, WIFI_SSID, WIFI_PASSWORD);
   Firebase.reconnectWiFi(true);
 
-  if (Firebase.setInt(fbdo, "/int", 1))
+  StaticJsonDocument<512> doc;
+  JsonArray shelves = doc.createNestedArray("arduino 1");
+  JsonObject shelf1 = shelves.createNestedObject();
+  JsonObject shelf2 = shelves.createNestedObject();
+
+  shelf1["shelfId"] = "shelf 1";
+  shelf1["contents"] = 0;
+  shelf1["capacity"] = 2;
+
+  shelf2["shelfId"] = "shelf 2";
+  shelf2["contents"] = 0;
+  shelf2["capacity"] = 3;
+
+  serializeJson(doc, jsonData);
+
+  if (Firebase.setJSON(fbdo, "/", jsonData))
   {
-    if (fbdo.dataType() == "int")
-      Serial.println(fbdo.intData());
+    if (fbdo.dataType() == "json")
+      Serial.println(fbdo.jsonData());
   }
   else
   {
     // Failed, then print out the error detail
     Serial.println(fbdo.errorReason());
   }
-
-  // StaticJsonDocument<512> doc;
-  // doc["arduinoId"] = "arduino 1";
-  // JsonArray shelves = doc.createNestedArray("shelves");
-  // JsonObject shelf1 = shelves.createNestedObject();
-  // JsonObject shelf2 = shelves.createNestedObject();
-
-  // shelf1["shelfId"] = "P60S9yHK54mJKG0nm3Jj";
-  // shelf1["contents"] = 0;
-  // shelf1["capacity"] = 2;
-
-  // shelf2["shelfId"] = "shelf 2";
-  // shelf2["contents"] = 0;
-  // shelf2["capacity"] = 3;
-
-  // serializeJson(doc, Serial);
-  // Serial.println();
 }
 
 void loop()
 {
-  
-  /*
-  // read the value from the sensor:
-  cur = analogRead(sensorPin);
 
-  if (cur != prev)
-  {
-    Serial.println(cur);
-    prev = cur;
-  }
-  delay(200);
-  */
 }
